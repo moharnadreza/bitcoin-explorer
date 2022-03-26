4c4ccfc2486921d06c188b62d22420563617e80e
Mohammadreza <moharnadreza@gmail.com>
2022-03-26 18:09:33 +0430

    feat: add button component

---
+++ components/Button/constants.ts
@@ -0,0 +1,6 @@
+import type { ButtonIconSize, ButtonSize } from './types';
+
+export const buttonIconSize: Record<ButtonSize, ButtonIconSize> = {
+  small: { width: 18, height: 18 },
+  medium: { width: 24, height: 24 },
+};
